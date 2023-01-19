db84be41d285f68f81467ac47e79bbd2b25b2d5e
Sparks <43890328+csparks78@users.noreply.github.com>
2023-01-19 09:30:32 -0700

    Initial push

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,86 @@
+import './index.scss'
+import { useState } from 'react'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+    faLinkedin,
+    faGithub,
+} from '@fortawesome/free-brands-svg-icons'
+import {
+    faHome,
+    faUser,
+    faEnvelope,
+    faBars
+} from '@fortawesome/free-solid-svg-icons'
+
+import { Link, NavLink } from 'react-router-dom'
+const Sidebar = () => {
+    const [showNav, setShowNav] = useState(false);
+    return (
+        <div className="nav-bar">
+            <Link className="logo"
+                to="/">
+
+                <img src={LogoS} alt="Logo" />
+                <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+            </Link>
+            <nav>
+                <NavLink
+                    exact="true"
+                    activeclassname="active"
+                    to="/"
+                    onClick={() => setShowNav(false)}>
+                    <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+                </NavLink>
+                <NavLink
+                    exact="true"
+                    activeclassname="active"
+                    to="/"
+                    onClick={() => setShowNav(false)}>
+                    <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+                </NavLink>
+                <NavLink
+                    exact="true"
+                    activeclassname="active"
+                    to="/"
+                    onClick={() => setShowNav(false)}>
+                    <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+                </NavLink>
+            </nav>
+            <ul>
+                <li>
+                    <a
+                        href="https://www.linkedin.com/in/cliff-sparks-8231092b/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <FontAwesomeIcon
+                            icon={faLinkedin}
+                            color="#4d4d4e"
+                            className="anchor-icon"
+                        />
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="https://github.com/csparks78"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <FontAwesomeIcon
+                            icon={faGithub}
+                            color="#4d4d4e"
+                            className="anchor-icon"
+                        />
+                    </a>
+                </li>
+            </ul>
+
+        </div>
+    )
+}
+
+
+
+export default Sidebar
