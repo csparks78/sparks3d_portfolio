ce5b1c4b0296d3f0411830ca6dec5078cf65c3ac
Sparks <43890328+csparks78@users.noreply.github.com>
2023-01-19 17:38:47 -0700

    added code

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,21 +4,22 @@ import { Link } from 'react-router-dom'
 import './index.scss'
 
 const Home = () => {
-    
-    return(
+
+    return (
 
         <div className="container home-page">
-             <div className="text-zone">
-                <h1>Hello, <br /> I'm</h1>
-                <img src={C_logo} alt='developer' />
-                Cliff
-                 <img src={S_logo} alt='developer' />
-                 Sparks
-                <br />
-                aspiring web developer
+            <div className="text-zone">
+                <h1>Hello, <br /> I'm
+                    <img className="C" src={C_logo} alt='developer' />
+                    liff
+                    <img src={S_logo} alt='developer' />
+                    parks
+                    <br />
+                    aspiring web developer
+                </h1>
                 <h2> Recently graduated Computer Science student</h2>
-                <Link to="/contact" className='flat-button'>CONTACT</Link>
-                </div>   
+                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+            </div>
         </div>
 
 
