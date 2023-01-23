524757483310240877819fa6bb922057bf06d2c2
Sparks <43890328+csparks78@users.noreply.github.com>
2023-01-23 09:31:38 -0700

    added code

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import resume from './Sparks_resume3.png'
+import { Link } from 'react-router-dom'
 import './index.scss'
 
 const Resume = () => {
 
   return (
-    <div className="resume-wrapper">
-      <img className="resume" src={resume} alt="resume" />
+    <div>
+    <img className="resume" src={resume} alt="resume" ></img>
+    <button>Download</button>
     </div>
   )
 }
