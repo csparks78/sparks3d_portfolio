3062c23183694652d8572c60fa5cc303e401f6ee
Sparks <43890328+csparks78@users.noreply.github.com>
2023-01-26 19:17:25 -0700

    added code

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -25,8 +25,8 @@ const Home = () => {
                     an aspiring web developer.
                 </h1>
                 <h2> Recently graduated Computer Science student</h2>
-                <Link to="/contact" className='flat-button-contact'>CONTACT ME</Link>
-                <Link to="/resume" className='flat-button-resume'>RESUME</Link>
+                <Link to="/contact" className='button flat-button-contact'>CONTACT ME</Link>
+                <Link to="/resume" className='button flat-button-resume'>RESUME</Link>
             </div>
         </div>
     )
--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import resume from './Sparks_resume3.png'
-import { Link } from 'react-router-dom'
 import './index.scss'
 
-const Resume = () => {
 
+
+const Resume = () => {
+  
+    
   return (
     <>
     <img className="resume" src={resume} alt="resume" ></img>
     
-    <button className='download-button'>Download</button>
-    
+    <button className='button'> Download </button>
+  
     </>
     
   )
